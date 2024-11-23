aee1bd467f26ff7cd420c4c4504bca7346f549c7
Omar Assidi <oia05@mail.aub.edu>
2024-11-23 18:03:38 +0300

    added play sound functionality

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -10,30 +10,24 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
     
-    var player: AVAudioPlayer?
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+            
+        }
+        playSound(soundName: sender.titleLabel?.text ?? "")
+    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        guard url != nil else { return }
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
     }
 
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        playSound(fileName: sender.titleLabel?.text ?? "C")
-        sender.alpha = 0.5
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 , execute: {sender.alpha = 1})
-    }
-    
-    func playSound(fileName: String) {
-        guard let url = Bundle.main.url(forResource: fileName, withExtension: "wav") else { return }
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            guard let player = player else { return }
-            player.play()
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
 }
 
